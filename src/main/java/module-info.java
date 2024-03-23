27e8ce27e46bc7f4f54c62833834ef0999c8d584
Denis-Cosmin NUTIU <dnutiu@bitdefender.com>
2024-03-23 22:04:03 +0200

    add ikonli dependencies to module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,10 +7,12 @@ module com.nuculabs.dev.imagetagger.ui {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
     requires com.microsoft.onnxruntime;
     requires java.logging;
     requires java.desktop;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.core;
+    requires org.kordamp.ikonli.fontawesome5;
     requires kotlinx.coroutines.core;
 
     opens com.nuculabs.dev.imagetagger.ui to javafx.fxml, javafx.graphics;
