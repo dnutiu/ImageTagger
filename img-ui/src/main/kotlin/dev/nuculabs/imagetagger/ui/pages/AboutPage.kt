74d0bb5fe7aa7b6990ee01bd2977e6421c5051e4
Denis-Cosmin NUTIU <dnutiu@bitdefender.com>
2024-04-07 17:50:25 +0300

    add a close button

--- img-ui/src/main/kotlin/dev/nuculabs/imagetagger/ui/pages/AboutPage.kt
+++ img-ui/src/main/kotlin/dev/nuculabs/imagetagger/ui/pages/AboutPage.kt
@@ -1,15 +1,18 @@
 package dev.nuculabs.imagetagger.ui.pages
 
+import javafx.application.Platform
 import javafx.fxml.FXML
 import javafx.fxml.FXMLLoader
 import javafx.scene.Parent
 import javafx.scene.Scene
+import javafx.scene.control.Button
 import javafx.stage.Stage
 import java.awt.Desktop
 import java.net.URL
 
 
 class AboutPage {
+    lateinit var closeButton: Button
 
     @FXML
     fun openBlog() {
@@ -19,6 +22,15 @@ class AboutPage {
     @FXML
     fun openGithub() {
         Desktop.getDesktop().browse(URL("https://github.com/dnutiu/ImageTagger").toURI());
+    }
+
+    @FXML
+    fun closePage() {
+        Platform.runLater {
+            // Get a reference to the stage
+            val stage = closeButton.scene.window as Stage
+            stage.hide()
+        }
     }
 
     companion object {
