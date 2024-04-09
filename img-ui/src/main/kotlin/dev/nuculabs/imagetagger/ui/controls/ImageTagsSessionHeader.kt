87339f853eb6f5c2c2ce2e5d5ab66ae204f105de
Denis-Cosmin NUTIU <dnutiu@bitdefender.com>
2024-04-10 00:17:01 +0300

    implement ImageTagsSessionHeader feature

--- img-ui/src/main/kotlin/dev/nuculabs/imagetagger/ui/controls/ImageTagsSessionHeader.kt
+++ img-ui/src/main/kotlin/dev/nuculabs/imagetagger/ui/controls/ImageTagsSessionHeader.kt
@@ -1,10 +1,37 @@
 package dev.nuculabs.imagetagger.ui.controls
 
+import dev.nuculabs.imagetagger.ui.alerts.ErrorAlert
+import dev.nuculabs.imagetagger.ui.utils.date.DateTimeProvider
+import dev.nuculabs.imagetagger.ui.utils.date.IDateTimeProvider
+import javafx.fxml.FXML
 import javafx.fxml.FXMLLoader
+import javafx.scene.control.Button
+import javafx.scene.control.Label
 import javafx.scene.layout.HBox
+import java.awt.Desktop
+import java.io.File
 import java.io.IOException
+import java.util.logging.Logger
 
 class ImageTagsSessionHeader : HBox() {
+    private val logger: Logger = Logger.getLogger("ImageTagsSessionHeader")
+
+    /**
+     * Is a provider for providing the date-time data for setting the title's header.
+     */
+    var dateTimeProvider: IDateTimeProvider = DateTimeProvider()
+
+    /**
+     * Is the path that will be opened when the open directory button is clicked.
+     */
+    private var directoryPath: String? = null
+
+    @FXML
+    private lateinit var title: Label
+
+    @FXML
+    private lateinit var openDirectoryButton: Button
+
     init {
         val fxmlLoader = FXMLLoader(
             ImageTagsSessionHeader::class.java.getResource("image-tags-session-header.fxml")
@@ -15,6 +42,45 @@ class ImageTagsSessionHeader : HBox() {
             fxmlLoader.load()
         } catch (exception: IOException) {
             throw RuntimeException(exception)
+        }
+
+        updateHeader(0)
+    }
+
+    /**
+     * Updates the header with the number of images and the directory path.
+     */
+    fun updateHeader(numberOfImages: Int, directoryPath: String? = null) {
+        // set directory path and button visibility
+        this.directoryPath = directoryPath
+        if (this.directoryPath == null) {
+            this.openDirectoryButton.isVisible = false
+        } else {
+            this.openDirectoryButton.isVisible = true
+        }
+
+        // update header title
+        val shortDate = dateTimeProvider.getTodayShortDate()
+        this.title.text = "$shortDate ($numberOfImages Images)"
+    }
+
+    /**
+     * Opens the directory in the user's Desktop.
+     */
+    fun openDirectory() {
+        this.directoryPath?.let {
+            if (Desktop.isDesktopSupported()) {
+                val desktop = Desktop.getDesktop()
+                if (desktop.isSupported(Desktop.Action.OPEN)) {
+                    desktop.open(File(it))
+                } else {
+                    logger.severe("Cannot open image directory $it. Desktop action not supported!")
+                    ErrorAlert("Can't open file: $it\nOperation is not supported!")
+                }
+            } else {
+                logger.severe("Cannot open image directory $it. Desktop action not supported!")
+                ErrorAlert("Can't open file: $it\nOperation is not supported!")
+            }
         }
     }
 }
